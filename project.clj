eec92e5178968f79fdb87ad724a78b1adfa4d472
Anders Janmyr <anders@janmyr.com>
2013-11-09 12:39:27 +0100

    removed uberjar crap and added midje plugin

--- project.clj
+++ project.clj
@@ -3,11 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}}
   :dependencies [
                  [org.clojure/clojure "1.5.1"]
                  [midje "1.5.1"]
                  [org.clojure/core.logic "0.8.4"]
-                 ]
-  :main ^:skip-aot mastermind-clj.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 ])
